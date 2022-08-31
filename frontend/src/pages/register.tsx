479f986deaccd2e5f89c1b26065781143c9c8c0c
jbyomuhangi <jbyomuhangi@gmail.com>
2022-08-31 11:42:54 +0800

    Handle register flow in frontend

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,21 +1,32 @@
 import React from "react";
 import { Formik, Form } from "formik";
 import { Box, Button } from "@chakra-ui/react";
+import { useRouter } from "next/router";
 
 import Wrapper from "../components/Wrapper";
 import InputField from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
 
 interface registerProps {}
 
 const register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
   const [, register] = useRegisterMutation();
 
   return (
     <Wrapper variant="regular">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => register(values)}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await register(values);
+
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            router.push("/");
+          }
+        }}
       >
         {({ isSubmitting }) => {
           return (
