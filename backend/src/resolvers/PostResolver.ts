523128a0f82f0faa50b9802f72c0df4f833cfe25
jbyomuhangi <jbyomuhangi@gmail.com>
2022-09-14 19:01:17 +0800

    Backend - added FieldResolver to Posts entity

--- backend/src/resolvers/PostResolver.ts
+++ backend/src/resolvers/PostResolver.ts
@@ -8,6 +8,8 @@ import {
   Field,
   UseMiddleware,
   Ctx,
+  FieldResolver,
+  Root,
 } from "type-graphql";
 
 import { Post } from "../entities/PostEntity";
@@ -23,8 +25,13 @@ class PostInput {
   text: string;
 }
 
-@Resolver()
+@Resolver(Post)
 export class PostResolver {
+  @FieldResolver(() => String)
+  textSnippet(@Root() root: Post) {
+    return root.text.slice(0, 50);
+  }
+
   @Query(() => [Post])
   async posts(
     @Arg("limit", () => Int, { defaultValue: 20, nullable: true })
