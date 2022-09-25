d79c3bdffb168d34de15f08902b305c6ac932b90
jbyomuhangi <jbyomuhangi@gmail.com>
2022-09-25 15:27:20 +0800

    Backend - improved on update post mutation

--- backend/src/resolvers/PostResolver.ts
+++ backend/src/resolvers/PostResolver.ts
@@ -170,21 +170,25 @@ export class PostResolver {
   }
 
   @Mutation(() => Post, { nullable: true })
+  @UseMiddleware(isAuth)
   async updatePost(
     @Arg("id", () => Int) id: number,
-    @Arg("title", () => String) title: string
+    @Arg("title", () => String) title: string,
+    @Arg("text", () => String) text: string,
+    @Ctx() { req, dataSource }: MyContext
   ): Promise<Post | null> {
-    const post = await Post.findOne({ where: { id } });
+    const result = await dataSource
+      .createQueryBuilder()
+      .update(Post)
+      .set({ title, text })
+      .where(`id = :id and "creatorId" = :creatorId`, {
+        id,
+        creatorId: req.session.userId,
+      })
+      .returning("*")
+      .execute();
 
-    if (!post) {
-      return null;
-    }
-
-    if (typeof title !== undefined) {
-      await Post.update({ id }, { title });
-    }
-
-    return post;
+    return result.raw[0];
   }
 
   @Mutation(() => Boolean)
